ed846d11aa8a569b56656c8f949adce19c02c5b0
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-04-20 08:37:48 +0500

    Add files via upload

---
+++ Q55.js
@@ -0,0 +1,5 @@
+"use strict";
+// Double Numbers in an Array: Make a list of numbers. Then, use a trick to make a new list where each number is twice its original value.
+let numbers = [1, 2, 3, 4, 5, 6];
+let doubleNumbers = numbers.map(number => number * 3);
+console.log(doubleNumbers);
